28bf65a9b08f460b52c8cbc930c73b50567a5a28
Mike Jaime <mikejaime@yahoo.com>
2014-05-22 18:38:20 -0500

    implemented form placeholder, post, added name and value,changed input type to button

--- public/form.php
+++ public/form.php
@@ -5,20 +5,20 @@
 </head>
 <body>
 	<?php 
-		var_dump($_GET);
-		var_dump($_POST);
+	var_dump($_GET);
+	var_dump($_POST);
 	?>
-	<form method="POST" action="">
+	<form method="GET" action="">
     <p>
         <label for="username">Username</label>
-        <input id="username" name="username" type="text">
+        <input id="username" name="username" method="post" type="text" placeholder="username">
     </p>
     <p>
         <label for="password">Password</label>
-        <input id="password" name="password" type="password">
+        <input id="password" name="password" type="password" placeholder="password">
     </p>
     <p>
-        <input type="submit">
+        <button type="submit">login</button>
     </p>
 </form>
 </body>
